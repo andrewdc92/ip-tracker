36bffdbd7ec9d711129f08dbefb6e37eb2276b5a
Andrew Cordivari <andrewcord@William-Cordivaris-MacBook-Pro.local>
2016-09-21 15:00:21 -0700

    WORKING YESYESYES

--- controllers/ipController.js
+++ controllers/ipController.js
@@ -1,2 +1,7 @@
 angular.module('ipApp')
   .controller('ipController', ipController);
+
+  function ipController() {
+      var vm = this;
+      vm.ip = "38.140.30.202"
+  };
--- scripts/directives/ip.js
+++ scripts/directives/ip.js
@@ -3,12 +3,36 @@ angular.module('ipApp')
 
   function ipAddress(){
     var directive = {
-      scope: { }
+      scope: {},
       restrict: 'E',
-      templateUrl: './templates/ipCardTemplate.html',
+      templateUrl: 'templates/ipCardTemplate.html',
       replace: true,
       controller: ipDirController,
       controllerAs: 'ipDirController'
     };
+
+
+  ipDirController.$inject=['$http'];
+  function ipDirController($http){
+    var vm = this;
+    var url="http://freegeoip.net/json/";
+    console.log(vm.data);
+    vm.getData = function(data){
+      $http({
+        method:'GET',
+        url: url
+      }).then(function(response){
+        console.log(response);
+        vm.data = response.data;
+      }, function(error){
+        console.log(error);
+      });
+
+    };
+    vm.getData(vm.data);
+  };
+
+
+    return directive
   }
-}
+  // response is not different than data in this case, no?
